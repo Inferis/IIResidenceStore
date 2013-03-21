61b88ce96483a1ee2cd7d09b81f4bf0220bca425
Inferis <spam@inferis.org>
2013-03-21 20:10:06 +0100

    Added beginnings of store demo.

---
+++ ResidenceStoreDemo/ResidenceStoreDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ResidenceStoreDemo
+//
+//  Created by Tom Adriaenssen on 21/03/13.
+//  Copyright (c) 2013 Tom Adriaenssen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ResidenceStoreDemo/ResidenceStoreDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ResidenceStoreDemo
+//
+//  Created by Tom Adriaenssen on 21/03/13.
+//  Copyright (c) 2013 Tom Adriaenssen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
